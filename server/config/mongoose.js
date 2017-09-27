fa08f4410064d1fe032682a2c74494568328afab
Branaman <MBranaman89@gmail.com>
2017-09-27 14:30:22 -0700

    added options to mongoose.connect

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
-mongoose.connect('mongodb://localhost/bikeMarket');
+var options = {};
+mongoose.connect('mongodb://localhost/bikeMarket', options);
 var models_path =path.join( __dirname , '/../models');
 fs.readdirSync(models_path).forEach(function(file) {
   if(file.indexOf('.js') > 0) {
